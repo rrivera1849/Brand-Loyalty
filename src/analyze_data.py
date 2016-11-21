191efb6874608135d6db2fcb9667e03b59524c4e
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2016-11-21 14:26:53 -0800

    Refactored code, now can run models in a generic fashion.

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -5,11 +5,10 @@ from optparse import OptionGroup, OptionParser
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 
 def CreateAllProductHistograms ():
@@ -23,86 +22,58 @@ def CreateAllProductHistograms ():
       print ('Processing Product Code %d' % pc)
       data_preprocessing.GetHouseholdProductFrequencyByCode (int (pc), True)
 
-
-def FitLogisticRegression (productCode, cutOff):
-  """Fits a logistic regression model and then evaluates how well
-     we did using k-fold validation.
-
-  Keyword Arguments:
-  productCode - string or integer representing product codes to use
-  cutOff - what brand non-brand cutoff to use when creating our labels
-  """
-  print ('Running Logistic Regression')
-  (X, Y) = data_preprocessing.PrepareData (productCode, cutOff)
-
-  logreg = LogisticRegression ()
-  scores = cross_val_score (logreg, X, Y.ratio.ravel (), cv=10)
-
-  return scores
-
-def FitSVM (productCode, cutOff, kernel='rbf'):
-  """Fits a support vector machine model and then evaluates how well
-     we did using k-fold validation.
+def FitModels (models, productCode, cutOff, kFold=10, doPlot=True):
+  """Fit an arbitrary number of models to the data and evaluate their
+     performance in a boxplot.
 
   Keyword Arguments:
-  productCode - string or integer representing product codes to use
-  cutOff - what brand non-brand cutoff to use when creating our labels
+  models - List of models to fit to the data.
+  productCode - products to evaluate the models on
+  cutOff - the point at which we determine someone to be branded or non-branded
+  kFold - number of folds to use for cross validation strategy
+  doPlot - whether we do the boxplot or not
   """
-  print ('Runnning SVM')
   (X, Y) = data_preprocessing.PrepareData (productCode, cutOff)
 
-  clf = SVC (kernel=kernel)
-  scores = cross_val_score (clf, X, Y.ratio.ravel (), cv=10)
+  names = []
+  results = []
 
-  return scores
+  for name, model in models:
+    scores = cross_val_score (model, X, Y.ratio.ravel (), cv=kFold)
+    results.append (scores)
+    names.append (name)
+    msg = "%s: %f (%f)" % (name, scores.mean(), scores.std())
+    print (msg)
 
-def FitAdaBoost (productCode, cutOff):
-  """Fits a boosting model model and then evaluates how well
-     we did using k-fold validation.
-
-  Keyword Arguments:
-  productCode - string or integer representing product codes to use
-  cutOff - what brand non-brand cutoff to use when creating our labels
-  """
-  print ('Runnning AdaBoost')
-  (X, Y) = data_preprocessing.PrepareData (productCode, cutOff)
-
-  bdt = AdaBoostClassifier (n_estimators=200)
-  scores = cross_val_score (bdt, X, Y.ratio.ravel (), cv=10)
-
-  return scores
-
-def FitSVMGrid (productCode, cutOff):
-  """Attempts to find the best parameters for a support vector machine model.
-
-  Keyword Arguments:
-  productCode - string or integer representing product codes to use
-  cutOff - what brand non-brand cutoff to use when creating our labels
-  """
-  print ('Running SVM Grid...')
-  (X, Y) = data_preprocessing.PrepareData (productCode, cutOff)
-
-  C_range = np.logspace(-3, 3, 5)
-  gamma_range = np.logspace(-3, 3, 5)
-  param_grid = dict(gamma=gamma_range, C=C_range)
-
-  cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=43)
-  grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv)
-  grid.fit (X, Y.ratio.ravel ())
-
-  return grid
+  if doPlot:
+    fig = plt.figure ()
+    fig.suptitle('Algorithm Comparison')
+    plt.boxplot (results)
+    x = fig.add_subplot (111)
+    x.set_xticklabels (names)
+    plt.show ()
 
 def main ():
   parser = OptionParser ()
 
-  parser.add_option ('--create-histograms', action='store_true', dest='create_histograms', default=False)
-  parser.add_option ('--create-single-histogram', action='store_true', dest='create_single_histogram', default=False)
-  parser.add_option ('-p', '--product-code', dest='product_code', type='string')
-  parser.add_option ('-c', '--cut-off', dest='cutoff', type=float)
-  parser.add_option ('--run-svm', action='store_true', dest='run_svm', default=False)
-  parser.add_option ('--run-svm-grid', action='store_true', dest='run_svm_grid', default=False)
-  parser.add_option ('--run-logistic', action='store_true', dest='run_logistic', default=False)
-  parser.add_option ('--run-ada-boost', action='store_true', dest='run_ada_boost', default=False)
+  generalGroup = OptionGroup (parser, "General Options", "These options may be requirements for any other options.") 
+  generalGroup.add_option ('-p', '--product-code', dest='product_code', type='string')
+  generalGroup.add_option ('-c', '--cut-off', dest='cutoff', type=float)
+
+  modelGroup = OptionGroup (parser, "Model Options", "These options control what models to run on the dataset.")
+  modelGroup.add_option ('--run-svm', action='store_true', dest='run_svm', default=False)
+  modelGroup.add_option ('--run-svm-grid', action='store_true', dest='run_svm_grid', default=False)
+  modelGroup.add_option ('--run-logistic', action='store_true', dest='run_logistic', default=False)
+  modelGroup.add_option ('--run-ada-boost', action='store_true', dest='run_ada_boost', default=False)
+
+  utilityGroup = OptionGroup (parser, "Utility Options", "These options are meant to provide extra functionality" \
+                              " such as creating histograms of the brand purchases for each household.")
+  utilityGroup.add_option ('--create-histograms', action='store_true', dest='create_histograms', default=False)
+  utilityGroup.add_option ('--create-single-histogram', action='store_true', dest='create_single_histogram', default=False)
+
+  parser.add_option_group (generalGroup)
+  parser.add_option_group (modelGroup)
+  parser.add_option_group (utilityGroup)
   
   (options, args) = parser.parse_args ()
 
@@ -123,21 +94,19 @@ def main ():
     if options.create_single_histogram:
       GetHouseholdProductFrequencyByCode (options.product_code, True)
 
-    if options.run_svm == True:
-      scores = FitSVM (options.product_code, options.cutoff)
-      print ("The mean accuracy of svm is %0.2f" % scores.mean ())
+    models = []
 
-    if options.run_svm_grid == True:
-      svmGrid = FitSVMGrid (options.product_code, options.cutoff)
-      print("The best parameters are %s with a score of %0.2f" % (svmGrid.best_params_, svmGrid.best_score_))
+    if options.run_svm == True:
+      models.append (('SVM', SVC ()))
 
     if options.run_logistic == True:
-      scores = FitLogisticRegression (options.product_code, options.cutoff)
-      print ("The mean accuracy of logistic regression is %0.2f" % scores.mean ())
+      models.append (('Logistic Regression', LogisticRegression ()))
 
     if options.run_ada_boost == True:
-      scores = FitAdaBoost (options.product_code, options.cutoff)
-      print ("The mean accuracy of ada boost is %0.2f" % scores.mean ())
+      models.append (('AdaBoost', AdaBoostClassifier (n_estimators=200)))
+
+    if len (models) > 0:
+      FitModels (models, options.product_code, options.cutoff, 10, True)
 
 if __name__ == "__main__":
   main ()
