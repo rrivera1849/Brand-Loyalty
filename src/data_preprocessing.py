1f58b9ebb6147df9832628d59d16369d8bfec9d8
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2016-11-21 16:04:14 -0800

    Fixed bug in feature extraction, had commented wrong lines.

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -98,12 +98,12 @@ def PrepareDataInternal (productCode):
   # Convert the following categorical varaibles to booleans
   # merged['age_and_presence_of_children'] = np.where (merged.age_and_presence_of_children == 9, 0, 1)
   merged[merged['age_and_presence_of_children'] == 9] = 0
-  merged['male_head_employment'] = np.where (merged.male_head_employment.isin ([1, 2, 3]), 1, 0)
-  merged['female_head_employment'] = np.where (merged.female_head_employment.isin ([1, 2, 3]), 1, 0)
+  # merged['male_head_employment'] = np.where (merged.male_head_employment.isin ([1, 2, 3]), 1, 0)
+  # merged['female_head_employment'] = np.where (merged.female_head_employment.isin ([1, 2, 3]), 1, 0)
   merged[merged['male_head_employment'] == 9] = 0
   merged[merged['female_head_employment'] == 9] = 0
-  # merged['hispanic_origin'] = np.where (merged.hispanic_origin == 1, 1, 0)
-  # merged['wic_indicator_current'] = np.where (merged.wic_indicator_current == 1, 1, 0)
+  merged['hispanic_origin'] = np.where (merged.hispanic_origin == 1, 1, 0)
+  merged['wic_indicator_current'] = np.where (merged.wic_indicator_current == 1, 1, 0)
 
   # Seperate out race into boolean variables
   merged['race_white'] = np.where (merged.race == 1, 1, 0)
